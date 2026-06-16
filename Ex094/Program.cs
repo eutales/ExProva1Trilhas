1c53fc44f08e3d6a1a9b08d9aa96034a1cdee862
eutales <eu.tsiqueira@gmail.com>
2026-06-16 15:03:50 -0300

    aaa

---
+++ Ex094/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("Exercício 94");
+int A, B;
+Console.WriteLine("Executar o conjunto 10 vezes: ");
+for (int i = 0; i < 2; i++)
+
+{
+
+    Console.Write("Digite um valor inteiro para A: ");
+    A = int.Parse(Console.ReadLine());
+    Console.Write("Digite um valor inteiro para B: ");
+    B = int.Parse(Console.ReadLine());
+    Console.WriteLine($"O resto da divisão de {A} por {B} é {A % B}");
+
+}
+
+Console.WriteLine(" Não executar nenhuma vez; ");
+for (int i = 0; i < 0; i++)
+
+{
+
+    Console.Write("Digite um valor inteiro para A: ");
+    A = int.Parse(Console.ReadLine());
+    Console.Write("Digite um valor inteiro para B: ");
+    B = int.Parse(Console.ReadLine());
+    Console.WriteLine($"O resto da divisão de {A} por {B} é {A % B}");
+
+}
+
+Console.WriteLine(" Executar o conjunto 100 vezes utilizando duas estruturas de repetição ");
+for (int i = 0; i < 2; i++)
+
+{
+    for (int x = 0; x < 50; x++)
+    {
+        Console.Write("Digite um valor inteiro para A: ");
+        A = int.Parse(Console.ReadLine());
+        Console.Write("Digite um valor inteiro para B: ");
+        B = int.Parse(Console.ReadLine());
+        Console.WriteLine($"O resto da divisão de {A} por {B} é {A % B}");
+    }
+}
+
+Console.WriteLine("Digite o número de vezes a ser executado:");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    Console.Write("Digite um valor inteiro para A: ");
+    A = int.Parse(Console.ReadLine());
+    Console.Write("Digite um valor inteiro para B: ");
+    B = int.Parse(Console.ReadLine());
+    Console.WriteLine($"O resto da divisão de {A} por {B} é {A % B}");
+}
+
+
---
+++ Ex89/Program.cs
@@ -0,0 +1,22 @@
+﻿int num = 0, soma = 0, numNeg = 0;
+
+
+
+
+
+for (int i = 1; i <= 4; i++)
+{
+    Console.Write("Digite um número: ");
+    num = int.Parse(Console.ReadLine());
+    if (num >= 0)
+    { 
+        soma = soma + num; //Acumulando 
+    }
+    else
+    {
+        numNeg = numNeg + 1; //Contando de 1 em 1
+    }
+}
+
+Console.WriteLine($"A soma dos números positivos {soma}");
+Console.WriteLine($"A contagem dos números negativos {numNeg}");
